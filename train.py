8c1e0af251cb5ab072ebacc1c8259a3c937dbaa4
suber <subercui@sina.com>
2019-04-10 10:31:43 +0800

    Reach acc 0.9355 Epoch: 0650 loss_train: 0.1289 -- loss_val: 0.1661 acc_val: 0.9355 time: 5.0739s

--- train.py
+++ train.py
@@ -22,15 +22,15 @@ parser.add_argument('--no-cuda', action='store_true', default=False, help='Disab
 parser.add_argument('--fastmode', action='store_true', default=False, help='Validate during training pass.')
 parser.add_argument('--sparse', action='store_true', default=False, help='GAT with sparse version or not.')
 parser.add_argument('--seed', type=int, default=72, help='Random seed.')
-parser.add_argument('--epochs', type=int, default=200, help='Number of epochs to train.')
+parser.add_argument('--epochs', type=int, default=1000, help='Number of epochs to train.')
 parser.add_argument('--batch', type=int, default=64, help='Number of samples in a mini batch.')
 parser.add_argument('--lr', type=float, default=0.001, help='Initial learning rate.')
 parser.add_argument('--weight_decay', type=float, default=5e-4, help='Weight decay (L2 loss on parameters).')
-parser.add_argument('--hidden', type=int, default=8, help='Number of hidden units.')
+parser.add_argument('--hidden', type=int, default=4, help='Number of hidden units.')
 parser.add_argument('--nb_heads', type=int, default=4, help='Number of head attentions.')
 parser.add_argument('--dropout', type=float, default=0.1, help='Dropout rate (1 - keep probability).')  # this is important
 parser.add_argument('--alpha', type=float, default=0.2, help='Alpha for the leaky_relu.')
-parser.add_argument('--patience', type=int, default=50, help='Patience')
+parser.add_argument('--patience', type=int, default=100, help='Patience')
 
 args = parser.parse_args()
 args.cuda = not args.no_cuda and torch.cuda.is_available()
